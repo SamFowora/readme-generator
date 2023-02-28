1d0afa1b40a8d90a0f0c6be40d2cd98076443e71
Sam Fowora <sffowora@yahoo.co.uk>
2023-02-28 07:26:39 +0000

    added questions and validation, gitignore for DS_Store and node_modules done

--- index.js
+++ index.js
@@ -4,9 +4,111 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+// project title, description, contents (just insert to generatemkd), installation, usage, licensing, contributing, tests, questions
+const questions = () => {
+    return inquirer.prompt([
 
-];
+    // project title
+    {
+        type: 'input',
+        name: ['title' + `README.md`],
+        message: 'Insert project title: ',
+        validate: (value) => {
+            if (value){return true}
+            else {return "Enter a title pretty please"}
+        }
+    },
+    
+    // project description
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Insert project description: ',
+        validate: (value)  => {
+            if (value){return true}
+            else {return "Come onnnn, enter a description"}
+        }
+    },
+    
+    // installing the project
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What steps are required to install your project: ',
+        validate: (value) => {
+            if (value){return true}
+            else {return "Pls...installation steps"}
+        }
+    },
+    
+    // how to use the app
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Insert instructions for project use: ',
+        validate: (value) => {
+            if (value){return true}
+            else {return "Usage instructions, I'm BEGGING!"}
+        }
+    },
+    
+    // license choice, could use dropdown through inquirer
+    {
+        type: 'input',
+        name: 'licensing',
+        message: 'Choose project license: ',
+        validate: (value) => {
+            if (value){return true}
+            else {return "Enter a license pretty please"}
+        }
+    },
+    
+    // project credits
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Insert collaborators/credits: ',
+        validate: (value) => {
+            if (value){return true}
+            else {return "Lone wolf?"}
+        }
+    },
+    
+    // tests?
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Insert tests: ',
+        validate: (value) => {
+            if (value){return true}
+            else {return "Tests please :)"}
+        }
+    },
+    
+    // questions, insert github AND email (how??) 
+    {
+        type: 'input',
+        name: 'GitHub username',
+        message: 'Insert GitHub link: ',
+        validate: (value)  => {
+            if (value){return true}
+            else {return "No commits on your GitHub or something? Where's the link??"}
+        }
+    },
+
+    // email 
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Insert email: ',
+        validate: (value)  => {
+            if (value){return true}
+            else {return "They need an email to reach out to you :( "}
+        }
+    }
+
+])
+};
 
 // function to write README file
 function writeToFile(fileName, data) {
